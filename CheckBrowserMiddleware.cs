1be9caa36e1010da902d4c54cfa0b0ce23a27e91
Gabriel Sawicki <80664@student.pb.edu.pl>
2022-05-24 15:40:13 +0200

    Dodaj pliki projektów.

---
+++ CheckBrowserMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using UAParser;
+
+namespace Middleware
+{
+    public class CheckBrowserMiddleware
+    {
+        private RequestDelegate _next;
+
+        public CheckBrowserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var Agent = context.Request.Headers.UserAgent;
+            var Parser = UAParser.Parser.GetDefault();
+            ClientInfo client = Parser.Parse(Agent);
+
+            if (client.UA.Family == "Edge" || client.UA.Family == "IE" || client.UA.Family == "EdgeChromium")
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+            }
+            await _next(context);
+        }
+    }
+}
